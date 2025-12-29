fd900c3db6863f27fee4e702401bce39f1a94a90
harshalmali7777 <harshalmali404040@gmail.com>
2025-12-29 21:39:16 +0530

    Added light and Dark theme

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,9 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -33,5 +36,23 @@ public class PageController
         public String servicesPage(){
             System.out.println("services page loading");
             return "services";
-        }
+    }
+
+    // contact
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+    
+    // login
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // register
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
 }

2326aa55e27a9391a18d7ce6b36d26a727e6d136
harshalmali7777 <harshalmali404040@gmail.com>
2026-02-16 15:59:25 +0530

    email verification is down

--- src/main/java/com/scm/repositories/UserRepo.java
+++ src/main/java/com/scm/repositories/UserRepo.java
@@ -13,5 +13,5 @@ public interface UserRepo extends JpaRepository<User, String>{
 
     Optional<User> findByEmailAndPassword(String email, String password);
 
-
+    Optional<User> findByEmailToken(String id);
 } 
